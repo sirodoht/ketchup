4d6e72b0184b98f5f5696fdfdb7852d488e1ca33
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-03-09 17:49:36 +0200

    make app to boot the main comp

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Timer from './Timer.comp';
+
+import './style.css';
+
+ReactDOM.render(<Timer />, document.getElementById('app'));
